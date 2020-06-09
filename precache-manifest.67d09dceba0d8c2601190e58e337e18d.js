b588fb30929115b743700009c63518266a403b17
hashedIngineer <58679711+hashedIngineer@users.noreply.github.com>
2020-06-09 09:31:30 +0530

    Updates

---
+++ precache-manifest.67d09dceba0d8c2601190e58e337e18d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a3978af11988b6f16f69a5cd8383e3",
+    "url": "/rickandmorty_catalog/index.html"
+  },
+  {
+    "revision": "fcf06e368fb674d418dd",
+    "url": "/rickandmorty_catalog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef2a9d1860f10bf02855",
+    "url": "/rickandmorty_catalog/static/css/main.9a677686.chunk.css"
+  },
+  {
+    "revision": "fcf06e368fb674d418dd",
+    "url": "/rickandmorty_catalog/static/js/2.c23fc61c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/rickandmorty_catalog/static/js/2.c23fc61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2a9d1860f10bf02855",
+    "url": "/rickandmorty_catalog/static/js/main.6e50a5cf.chunk.js"
+  },
+  {
+    "revision": "cdb0d856fe13c5f3d76d",
+    "url": "/rickandmorty_catalog/static/js/runtime-main.51d18881.js"
+  },
+  {
+    "revision": "3c26307a28e5487215fc6d3f15d25ea1",
+    "url": "/rickandmorty_catalog/static/media/Cartoon_Network_1992_logo.3c26307a.svg"
+  }
+]);
